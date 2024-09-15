2420c3d4429c2d53d60b8dbb89d0698ec4682570
najib67 <adibabdennaji@yahoo.fr>
2024-09-15 18:56:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,15 +40,18 @@ with st.sidebar:
          'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
+  #Encode
+  encode = ['islande', 'sex']
+  df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+  df_penguins[:1]
   
 with st.expander('Input features'):
   st.write('**Input penguin**')
   input_df
   st.write('**Combined penguins data**')
   input_penguins
+  st.write('**Encoded input penguin**')
+  input_row
 
-#Encode
-encode = ['islande', 'sex']
-df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-df_penguins[:1]
+
 

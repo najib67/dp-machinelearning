df4045cc026745e56d876a765ffe9fcb82788c92
najib67 <adibabdennaji@yahoo.fr>
2024-09-15 19:25:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,9 @@ df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 input_row = df_penguins[:1]
 
 # Encode y
-target_mapper = ('Adelie': 0,
+target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
-                 'Gentoo' :2)
+                 'Gentoo' :2)}
 def target_encode(val):
   return target_mapper[val]
 

fb270ab461d8188f984d0bbbae2bdf1508b68c86
najib67 <adibabdennaji@yahoo.fr>
2024-09-15 17:37:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,24 +10,33 @@ with st.expander('Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
 
-st.write('**X**')
-X = df.drop('species', axis=1)
-X
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  X
 
-st.write('**y**')
-y = df.species
-y
+  st.write('**y**')
+  y = df.species
+  y
 
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
 #Data preparation
-
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-  gender = st.selectbox('Gender', ('male', 'femal'))
   bill_length_mm = st.slider('Bull length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('bill depth (mm)', 13.0, 21.5, 17.2)
   flipper_lendth_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)',2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'femal'))
+  
+  #Create DataFrame for the input features
+  data = {'island', island,
+         'bill_length_mm', bill_length_mm,
+         'bill_depth_mm', bill_depth_mm,
+         'flipper_lendth_mm', flipper_lendth_mm,
+         'body_mass_g', body_mass_g,
+         'gender', gender}
+  input_df = pd.DataFramer(data, index[0])
+input_df

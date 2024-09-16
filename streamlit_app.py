0605caa36182385a3c2d4dfa264bec0b42d14851
najib67 <adibabdennaji@yahoo.fr>
2024-09-16 18:15:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,5 +82,9 @@ clf.fit(X, y)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
+prediction_proba.rename(columns={0: 'Adelie',
+                                 1: 'Chinstrap',
+                                 2: 'Gentoo'})
 prediction_proba
+
 

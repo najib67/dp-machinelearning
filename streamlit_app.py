4d375ba534468c68dbe94e227e2603fe97e98231
najib67 <adibabdennaji@yahoo.fr>
2024-09-16 19:28:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,12 +93,12 @@ df_prediction_proba.rename(columns={0: 'adelie',
 st.subheader('Predicted Species')
 st.dataframe(df_prediction_proba,
              column_config={
-               'Adelie': st.column_config.ProgressColumn(
-                 'Adelie',
+               "Adelie": st.column_config.ProgressColumn(
+                 "Adelie",
                  format='%f',
                  width='medium',
                  min_value=0,
-                 max_valus=1
+                 max_valus=1,
                 ),
                 'Chinstrap': st.column_config.ProgressColumn(
                  'Chinstrap',

e17af1c07542c5d7a761155b915cc9c1f59095aa
najib67 <adibabdennaji@yahoo.fr>
2024-09-16 18:42:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 st.title('🎈 Machine Learning App')
@@ -87,6 +88,10 @@ df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
 df_prediction_proba.rename(columns={0: 'adelie',
                                     1: 'Chinstrap',
                                     2: 'Gentoo'})
-df_prediction_proba
+# df_prediction_proba
 
+# Display predicted species
+st.subheader('Predicted Species')
+penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.success(str(penguins_species[prediction][0])
 

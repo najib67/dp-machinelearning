b04ac9ecc22a300a51b3894740546e04d8fa13bc
najib67 <adibabdennaji@yahoo.fr>
2024-09-17 17:36:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-st.title('🎈 Machine Learning App')
+st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
@@ -23,23 +23,23 @@ with st.expander('Data'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
-#Input features
+# Input features
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-  bill_length_mm = st.slider('Bull length (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('bill depth (mm)', 13.0, 21.5, 17.2)
-  flipper_lendth_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Body mass (g)',2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male', 'femal'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
   
-  #Create DataFrame for the input features
+  # Create a DataFrame for the input features
   data = {'island': island,
-         'bill_length_mm': bill_length_mm,
-         'bill_depth_mm': bill_depth_mm,
-         'flipper_lendth_mm': flipper_lendth_mm,
-         'body_mass_g': body_mass_g,
-         'sex': gender}
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
 
@@ -50,9 +50,9 @@ with st.expander('Input features'):
   input_penguins
 
 
-# Data preparation  
+# Data preparation
 # Encode X
-encode = ['islande', 'sex']
+encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 
 X = df_penguins[1:]
@@ -61,7 +61,7 @@ input_row = df_penguins[:1]
 # Encode y
 target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
-                 'Gentoo' :2}
+                 'Gentoo': 2}
 def target_encode(val):
   return target_mapper[val]
 
@@ -74,7 +74,7 @@ with st.expander('Data preparation'):
   y
 
 
-# model training and inference
+# Model training and inference
 ## Train the ML model
 clf = RandomForestClassifier()
 clf.fit(X, y)
@@ -85,9 +85,9 @@ prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
-df_prediction_proba.rename(columns={0: 'adelie',
-                                    1: 'Chinstrap',
-                                    2: 'Gentoo'})
+df_prediction_proba.rename(columns={0: 'Adelie',
+                                 1: 'Chinstrap',
+                                 2: 'Gentoo'})
 
 # Display predicted species
 st.subheader('Predicted Species')
@@ -98,26 +98,24 @@ st.dataframe(df_prediction_proba,
                  format='%f',
                  width='medium',
                  min_value=0,
-                 max_valus=1,
-                ),
-                'Chinstrap': st.column_config.ProgressColumn(
+                 max_value=1
+               ),
+               'Chinstrap': st.column_config.ProgressColumn(
                  'Chinstrap',
                  format='%f',
                  width='medium',
                  min_value=0,
-                 max_valus=1,
-                ),
-                'Gentoo': st.column_config.ProgressColumn(
+                 max_value=1
+               ),
+               'Gentoo': st.column_config.ProgressColumn(
                  'Gentoo',
                  format='%f',
                  width='medium',
                  min_value=0,
-                 max_valus=1,
-                ),
-              },
-              hide_index=True,
-          )
-              
+                 max_value=1
+               ),
+             }, hide_index=True)
+
+
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguins_species[prediction][0]))
-
